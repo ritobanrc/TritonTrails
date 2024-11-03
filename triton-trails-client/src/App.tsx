34c874b7315490b7d668a29608108bdbe2164ed9
AndrewLCA <andrewlu3002@gmail.com>
2024-11-03 13:01:33 -0800

    header

--- triton-trails-client/src/App.tsx
+++ triton-trails-client/src/App.tsx
@@ -4,21 +4,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    //<h1>Hello</h1>
+    <div className="Header">
+      <h1 className = "Title">Triton Trails</h1>
+      <img src="/topbanner.png" alt="Top banner" className= "width" />
+      
     </div>
   );
 }
