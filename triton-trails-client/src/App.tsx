d016c287164b61d294a903ab66977ebf9254d39a
Anna He <130006361+duomei98@users.noreply.github.com>
2024-11-03 17:56:25 -0800

    add search files w very very basic setup

--- triton-trails-client/src/App.tsx
+++ triton-trails-client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Search from './components/Search page/Search'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Navbar/>
+        <Navbar/>
+        <Search/>
       </header>
     </div>
   );
