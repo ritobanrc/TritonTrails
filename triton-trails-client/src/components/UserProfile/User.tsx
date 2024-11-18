f6c1e25c22706b4b746f4add33545c7f9d44357d
Misha-Boyko <mboykombo@gmail.com>
2024-11-18 14:42:47 -0800

    Finished integrating user auth backend with frontend #17. Session management not implemented

--- triton-trails-client/src/components/UserProfile/User.tsx
+++ triton-trails-client/src/components/UserProfile/User.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import './User.css';
 import { Link } from "react-router-dom";
+import { useAppContext } from '../../context/AppContext';
 
-const User = () => {
-    return (
-      <div className="user-profile">
-        <div className="avatar"></div>
-        <div className="user-info">
-          <h2>User1</h2>
+const UserProfile = () => {
+  const { user, setUser } = useAppContext();
+
+  if (!user) {
+    return <p>Please log in</p>;
+  }
+
+  return (
+    <div className="user-profile">
+      <div className="avatar"></div>
+      <div className="user-info">
+        <h2>{user.displayName}</h2>
           <a href="mailto:user1@ucsd.edu">user1@ucsd.edu</a>
-          <p>Short bio I suppose</p>
-        </div>
-        <div className="user-actions">
-            <Link to="/add-trail-form">
-                <button className="add-trail-button">Add Your Own Trail</button>
-            </Link>
-          <button className="edit-profile-button">Edit profile</button>
-          <button className="settings-button">Settings</button>
-        </div>
+        <p>Short bio I suppose</p>
       </div>
-    );
-  };
+      <div className="user-actions">
+          <Link to="/add-trail-form">
+              <button className="add-trail-button">Add Your Own Trail</button>
+          </Link>
+        <button className="edit-profile-button">Edit profile</button>
+        <button className="settings-button">Settings</button>
+      </div>
+    </div>
+  );
+};
 
-export default User;
+
+export default UserProfile;
