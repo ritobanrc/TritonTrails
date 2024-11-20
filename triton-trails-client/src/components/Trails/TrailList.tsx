6328d02be34b800ab62164b4f6cb484350f31a9c
Misha-Boyko <mboykombo@gmail.com>
2024-11-19 16:34:36 -0800

    Used flexbox to improve trail display on frontend to match the figma better

--- triton-trails-client/src/components/Trails/TrailList.tsx
+++ triton-trails-client/src/components/Trails/TrailList.tsx
@@ -28,46 +28,56 @@ const TrailDisplay: React.FC<{ trail: Trail }> = ({ trail }) => {
     return (
         <div className="rounded-div">
             <p className="trail-name">{trail.name}</p>
-            {images.length > 0 && (
-                <img
-                    className="trail-image"
-                    src={`${API_BASE_URL}/image/` + images[0]} // Display the first image
-                    alt={trail.name}
-                />
-            )}
-            <p className="trail-description">{trail.description}</p>
+            <div className="trail-details">
+                <div className="trail-visuals">  {/* Container for image and map only */}
+                    <div className="trail-content">
+                        {images.length > 0 && (
+                            <img
+                                className="trail-image"
+                                src={`${API_BASE_URL}/image/` + images[0]}
+                                alt={trail.name}
+                            />
+                        )}
+                    </div>
+                    <div className="trail-map">
+                        <iframe
+                            src="https://www.openstreetmap.org/export/embed.html?bbox=-117.26150035858156%2C32.86383591013185%2C-117.23493576049806%2C32.891227612173246&layer=mapnik"
+                            title="Trail Map"
+                            style={{ width: '100%', height: '100%', border: 'none' }}
+                        ></iframe>
+                    </div>
+                </div>
+                <p className="trail-description">{trail.description}</p> {/* Separate description */}
+            </div>
         </div>
     );
 };
 
 const TrailList = () => {
-  const { trails, setTrails } = useContext(AppContext);
+    const { trails, setTrails } = useContext(AppContext);
 
-  useEffect(() => {
-      loadTrails();
-  }, []);
+    useEffect(() => {
+        const loadTrails = async () => {
+            try {
+                const trailsList = await fetchTrails();
+                setTrails(trailsList);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        loadTrails();
+    }, []);
 
-  const loadTrails = async () => {
-      try {
-          const trailsList = await fetchTrails();
-          setTrails(trailsList);
-      } catch (err: any) {
-          console.log(err.message);
-      }
-  };
-  return (
-    <div className="box">
-        <div className="search-content">
-          <div className = "ListTrails">
-            <ul className="list-group">
-                {trails.map((trail: Trail) => (
-                    <TrailDisplay key={trail.id} trail={trail}  /> // Use JSX to render the component
+    return (
+        <div className="box">
+            <div className="ListTrails">
+                {trails.map((trail) => (
+                    <TrailDisplay key={trail.id} trail={trail} />
                 ))}
-            </ul>
-          </div>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default TrailList;
+
