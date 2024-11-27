1f1f868cf17eb5a3203959a49d49479ac899e702
Anna He <130006361+duomei98@users.noreply.github.com>
2024-11-27 01:02:38 -0800

    fixed map bug and added re-uploading image functionality

--- triton-trails-client/src/components/Trails/AddTrailForm.tsx
+++ triton-trails-client/src/components/Trails/AddTrailForm.tsx
@@ -1,12 +1,10 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
 import { AppContext } from "../../context/AppContext";
 import { Trail, Route } from "../../types/types";
 import { createTrail, createRoute } from "../../utils/trail-utils"; 
-
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import L from "leaflet";
 import './AddTrailForm.css';
-
 import { useNavigate } from 'react-router-dom';
 
 const defaultStartIcon = L.icon({
@@ -30,19 +28,17 @@ const AddTrailForm = () => {
   const [name, setName] = useState("");
   const [image, setImage] = useState("");
   const [description, setDescription] = useState("");
-
   const [startPoint, setStartPoint] = useState<[number, number] | null>(null);
   const [endPoint, setEndPoint] = useState<[number, number] | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isHovered, setIsHovered] = useState(false);
+  const navigate = useNavigate();
 
-  const [imageUploaded, setImageUploaded] = useState(false);
-  
-  const navigate = useNavigate(); // Get the navigate function from react-router-dom
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const newTrail: Trail = {
-      id: trails.length + 1,  // ID is auto-incremented in the backend
+      id: trails.length + 1,
       name: name,
       image: image,
       description: description,
@@ -56,43 +52,37 @@ const AddTrailForm = () => {
       TrailId: newTrail.id,
     };
 
-    setTrails([...trails, newTrail]); // Update local trails (can later be fetched from the database)
-    // Clear input fields
+    setTrails([...trails, newTrail]);
     setName("");
     setImage("");
     setDescription("");
     setStartPoint(null);
     setEndPoint(null);
-    createTrail(newTrail);
-    createRoute(newTrail.id, newRoute);
 
-    // go to search page after submission
+    await createTrail(newTrail);
+    await createRoute(newTrail.id, newRoute);
     navigate('/');
   };
 
-  // Custom map events to handle marker placement
   const MapClickHandler = () => {
     useMapEvents({
       click(event) {
         const { lat, lng } = event.latlng;
 
         if (!startPoint) {
-          setStartPoint([lat, lng]); // Set start point when clicked
+          setStartPoint([lat, lng]);
         } else if (!endPoint) {
-          setEndPoint([lat, lng]); // Set end point when clicked
+          setEndPoint([lat, lng]);
         }
       }
     });
     return null;
   };
 
-  // Trigger file input on button click
   const handleButtonClick = () => {
-    // add state so you can click an image after it's uploaded
-    if (!imageUploaded){
-      document.getElementById('fileInput')!.click();
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
     }
-    setImageUploaded(true);
   }
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -100,7 +90,7 @@ const AddTrailForm = () => {
       const file = event.target.files[0];
       const reader = new FileReader();
       reader.onloadend = () => {
-        setImage(reader.result as string); // Store base64 image
+        setImage(reader.result as string);
       };
       reader.readAsDataURL(file);
     }
@@ -115,7 +105,8 @@ const AddTrailForm = () => {
         <form className="rounded-div-content" onSubmit={onSubmit}>
           <div className="flex-row">
             <div className="flex-col">
-              <input className="input-field-2"
+              <input 
+                className="input-field-2"
                 type="text"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
@@ -125,6 +116,8 @@ const AddTrailForm = () => {
               <div
                 className="image-upload-container"
                 onClick={handleButtonClick}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
                 style={{
                   backgroundImage: image ? `url(${image})` : "none",
                   backgroundSize: "cover",
@@ -135,37 +128,41 @@ const AddTrailForm = () => {
                   <div className="input-wrapper">
                     <img src="https://cdn-icons-png.freepik.com/512/5008/5008022.png" alt="Upload Image" />
                     <label>Upload Image</label>
-                    <input 
-                      type="file" 
-                      id="fileInput" 
-                      onChange={handleImageChange} 
-                      style={{ display: 'none' }}
-                      accept="image/*" />
                   </div>
                 )}
+                {image && isHovered && (
+                  <div className="input-wrapper overlay-icon">
+                    <img src="https://cdn-icons-png.freepik.com/512/5008/5008022.png" alt="Re-upload" />
+                    <label>Re-upload Image?</label>
+                  </div>
+                )}
+                <input 
+                  type="file" 
+                  ref={fileInputRef} // Using ref here
+                  onChange={handleImageChange} 
+                  style={{ display: 'none' }}
+                  accept="image/*" 
+                />
               </div>
             </div>
             <div className="flex-col">
               <h2 className="header">Select the start and end points</h2>
               <div className="trail-map">
-                <MapContainer center={[32.8638, -117.2615]} zoom={13} scrollWheelZoom={false} style={{ height: "300px", width: "100%" }}>
+                <MapContainer center={[32.8638, -117.2615]} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
                   <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
                   />
                   <MapClickHandler />
-                  {startPoint && (
-                    <Marker position={startPoint} icon={defaultStartIcon}/>
-                  )}
-                  {endPoint && (
-                    <Marker position={endPoint} icon={defaultEndIcon}/>
-                  )}
+                  {startPoint && <Marker position={startPoint} icon={defaultStartIcon}/>}
+                  {endPoint && <Marker position={endPoint} icon={defaultEndIcon}/>}
                 </MapContainer>
               </div>
             </div>
           </div>
           <h2 className="desc-header">Tell us about your experience!</h2>
-          <textarea className="input-field-3"
+          <textarea
+            className="input-field-3"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
             placeholder="Consider including tips and helpful features"
